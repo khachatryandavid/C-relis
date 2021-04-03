8d26b43adab52e29e126f0afd1f0074f02ff8a84
David Khachatryan <76668130+david-khachatryan@users.noreply.github.com>
2021-04-04 03:41:13 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-    std::ifstream myfile("a code in C-realis.txt");
+    std::ifstream myfile("a code in C-relis.txt");
     if (myfile.fail())
     {
         std::cout << "Error: Can't find the text file!" << std::endl;
